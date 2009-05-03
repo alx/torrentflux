06fb7131c38acc697f699dc46ec81603c459b911
Alexandre Girard <alx.girard@gmail.com>
2009-05-03 19:15:09 +0200

    clean loadavg_osx

--- loadavg_osx.php
+++ loadavg_osx.php
@@ -1,13 +1,15 @@
 <?php
-		##################################
-		## TORRENTFLUX LOADAVG OSX HACK 
-		##################################
-		## MADE BY COUSINCOCAINE
-		## aka
-		## Dr.Apple
-		##################################
-		// $/usr/bin/uptime
-		// 10:00  up 87 days, 15:56, 3 users, load averages: 0.20 0.23 0.24
-	      $uptime_array = explode("load averages:", shell_exec('uptime'));
-             $loadavg_array = explode(" ",trim($uptime_array[1]));
-		?>
+	##################################
+	## TORRENTFLUX LOADAVG OSX HACK 
+	##################################
+	## MADE BY COUSINCOCAINE
+	## aka
+	## Dr.Apple
+	##################################
+	
+	// $/usr/bin/uptime
+	// 10:00  up 87 days, 15:56, 3 users, load averages: 0.20 0.23 0.24
+	
+	$uptime_array = explode("load averages:", shell_exec('uptime'));
+	$loadavg_array = explode(" ",trim($uptime_array[1]));
+?>
